5563b9b32cc53b1bf1190e49280f75e1fd2083c0
Vishwanath <vishwanath.shankar@omnicell.com>
2023-02-03 10:12:51 +0530

    Initial commit

---
+++ src/test/java/com/example/javatpoint/SpringbootHandsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javatpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHandsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
